df9208f318d88bcd4e6dd743fc9530160e1d8961
Kieran <kieransweeden@gmail.com>
2023-04-04 21:56:39 +0100

    Add filtering of search results data for search component

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,11 +1,12 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
+import { ChangeEventHandler, FC, useEffect, useState } from "react";
 
 type FormulaOneDriver = {
-  dateOfBirth: string;
   driverId: string;
+  givenName: string;
   familyName: string;
+  dateOfBirth: string;
   nationality: string;
   url: string;
 };
@@ -36,8 +37,28 @@ function getStoredDrivers() {
   return storedDrivers;
 }
 
+interface SearchResultsProps {
+  drivers: FormulaOneDriver[];
+}
+
+const SearchResults: FC<SearchResultsProps> = ({ drivers }) => {
+  return (
+    <ul>
+      {drivers.map((driver: FormulaOneDriver) => (
+        <li key={driver.driverId}>{driver.familyName}</li>
+      ))}
+    </ul>
+  );
+};
+
 export default function Search() {
-  let [drivers, setDrivers] = useState<FormulaOneDriver[]>([]);
+  const [drivers, setDrivers] = useState<FormulaOneDriver[]>([]);
+  const [filteredDrivers, setFilteredDrivers] = useState<FormulaOneDriver[]>(
+    []
+  );
+  const [searchInputValue, setSearchInputValue] = useState("");
+  const [showSearchResultsComponent, setShowSearchResultsComponent] =
+    useState(false);
 
   useEffect(() => {
     async function getDrivers() {
@@ -51,19 +72,46 @@ export default function Search() {
     getDrivers();
   }, []);
 
+  const handleInputChange: ChangeEventHandler = (event) => {
+    const searchInput = event.target as HTMLInputElement;
+    const searchValue = searchInput.value.trim();
+
+    if (searchValue) {
+      const matchingDrivers = drivers.filter(({ givenName, familyName }) => {
+        return [givenName, familyName].some((field) =>
+          field.toLowerCase().includes(searchValue)
+        );
+      });
+
+      setFilteredDrivers(matchingDrivers);
+      setShowSearchResultsComponent(true);
+    } else {
+      setShowSearchResultsComponent(false);
+      setFilteredDrivers(drivers);
+    }
+
+    setSearchInputValue(searchValue);
+  };
+
   return (
-    <form className="w-full max-w-md mx-auto">
-      <div className="relative">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white" />
+    <section>
+      <form className="w-full max-w-md mx-auto">
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white" />
+          </div>
+          <input
+            type="search"
+            id="driver-search"
+            className="block w-full p-4 pl-10 text-sm text-slate-900 border border-slate-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search drivers"
+            onChange={handleInputChange}
+          />
         </div>
-        <input
-          type="search"
-          id="driver-search"
-          className="block w-full p-4 pl-10 text-sm text-slate-900 border border-slate-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search drivers"
-        />
-      </div>
-    </form>
+      </form>
+      {showSearchResultsComponent && (
+        <SearchResults drivers={filteredDrivers} />
+      )}
+    </section>
   );
 }
