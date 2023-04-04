161d33f104622e2d200ac3a9957f9dd21d61ab7e
Kieran <kieransweeden@gmail.com>
2023-04-04 19:50:13 +0100

    Add fetching and caching of driver data from api

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,78 @@
+"use client";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import { useState, useEffect, KeyboardEventHandler } from "react";
 
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const inter = Inter({ subsets: ["latin"] });
+
+type FormulaOneDriver = {
+  dateOfBirth: string;
+  driverId: string;
+  familyName: string;
+  nationality: string;
+  url: string;
+};
+
+type FormulaOneApiResponse = {
+  MRData: {
+    DriverTable: {
+      Drivers: FormulaOneDriver[];
+    };
+  };
+};
+
+async function fetchDrivers() {
+  console.log("Fetching drivers from db");
+  const response = await fetch(
+    "http://ergast.com/api/f1/drivers.json?limit=1000"
+  );
+  const data: FormulaOneApiResponse = await response.json();
+  const { Drivers: drivers } = data?.MRData?.DriverTable;
+  localStorage.setItem("drivers", JSON.stringify(drivers));
+  return drivers;
+}
+
+function getStoredDrivers() {
+  console.log("Fetching drivers from local storage");
+  let storedDrivers = localStorage.getItem("drivers");
+  if (storedDrivers) return JSON.parse(storedDrivers);
+  return storedDrivers;
+}
+
+export function Search() {
+  let [drivers, setDrivers] = useState<FormulaOneDriver[]>([]);
+
+  useEffect(() => {
+    async function getDrivers() {
+      let fetchedDrivers = getStoredDrivers();
+      if (!fetchedDrivers) {
+        fetchedDrivers = await fetchDrivers();
+      }
+      setDrivers(fetchedDrivers);
+    }
+
+    getDrivers();
+  }, []);
+
+  return (
+    <form className="w-full max-w-md mx-auto">
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white" />
+        </div>
+        <input
+          type="search"
+          id="driver-search"
+          className="block w-full p-4 pl-10 text-sm text-slate-900 border border-slate-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search drivers"
+        />
+      </div>
+    </form>
+  );
+}
 
 export default function Home() {
   return (
@@ -16,29 +84,7 @@ export default function Home() {
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
           Search for all things Formula 1
         </p>
-        <form className="w-full max-w-md mx-auto">
-          <label
-            htmlFor="default-email"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-          >
-            Email sign-up
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <FontAwesomeIcon
-                icon={faMagnifyingGlass}
-                className="text-white"
-              />
-            </div>
-            <input
-              type="email"
-              id="default-email"
-              className="block w-full p-4 pl-10 text-sm text-slate-900 border border-slate-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search for a driver, team or track..."
-              required
-            />
-          </div>
-        </form>
+        <Search />
       </div>
       <div className="bg-gradient-to-r from-slate-800 to-slate-900 w-full h-full absolute top-0 left-0 z-0"></div>
     </section>
